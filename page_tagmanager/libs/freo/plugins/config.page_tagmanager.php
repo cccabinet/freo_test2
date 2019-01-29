fd6d51c12fb8855197503bce616cbe7f744a7122
minakata <minakata.k-1005@holydragoon.jp>
2019-01-29 15:18:00 +0900

    管理画面からページタグを編集・修正すると、ログインしていない環境でページタグクラウドを閲覧した場合に、編集前のタグが表示されてしまうため、endファイルを追加し、configファイルにendファイルの読み込み設定を追加

---
+++ page_tagmanager/libs/freo/plugins/config.page_tagmanager.php
@@ -0,0 +1,27 @@
+<?php
+
+/*********************************************************************
+
+ ページタグ管理プラグイン | 設定ファイル (2019/01/29)
+
+ Copyright(C) 2009-2019 freo.jp
+
+*********************************************************************/
+
+//プラグインの名前
+define('FREO_PLUGIN_PAGE_TAGMANAGER_NAME', 'ページタグ管理');
+
+//プラグインのバージョン
+define('FREO_PLUGIN_PAGE_TAGMANAGER_VERSION', '1.0.2');
+
+//管理ページの設定
+define('FREO_PLUGIN_PAGE_TAGMANAGER_ADMIN', 'page_tagmanager/admin');
+
+//pageファイルの読み込み設定
+define('FREO_PLUGIN_PAGE_TAGMANAGER_LOAD_PAGE', 'page_tagmanager');
+
+//holydragoonjp 追加
+//endファイルの読み込み設定
+define('FREO_PLUGIN_PAGE_TAGMANAGER_LOAD_END', 'page_tagmanager/admin_post,page_tagmanager/admin_delete');
+
+?>
