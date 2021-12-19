e54c149d6122f99f12cd54eab6213600065eb3e0
minakata <minakata.k-1005@holydragoon.jp>
2021-12-19 10:13:17 +0900

    【修正】 php7以上で削除されたini_setを削除

---
+++ libs/freo/initialize.php
@@ -0,0 +1,37 @@
+<?php
+
+/*********************************************************************
+
+ freo | 初期化処理 (2012/09/30)
+
+ Copyright(C) 2009-2012 freo.jp
+
+*********************************************************************/
+
+//php7以上で削除されたini_setを削除 | holydragoonjp
+ini_set('default_charset', 'UTF-8');
+ini_set('mbstring.language', 'Japanese');
+ini_set('mbstring.substitute_character', 'none');
+
+ini_set('session.use_trans_sid', 0);
+ini_set('session.use_cookies', 1);
+ini_set('session.use_only_cookies', 0);
+ini_set('session.auto_start', 0);
+ini_set('session.cache_limiter', 'none');
+
+if (ini_get('register_globals')) {
+	exit('Fatal error');
+}
+if (ini_get('magic_quotes_gpc')) {
+	$_GET     = freo_stripslashes($_GET);
+	$_POST    = freo_stripslashes($_POST);
+	$_REQUEST = freo_stripslashes($_REQUEST);
+	$_SERVER  = freo_stripslashes($_SERVER);
+	$_COOKIE  = freo_stripslashes($_COOKIE);
+}
+
+if (ob_get_level()) {
+	ob_end_clean();
+}
+
+?>
