3e12ff584f7f8aca65018bacf790dc01c0edc150
cccabinet <chay84@hotmail.com>
2022-01-03 12:18:33 +0900

    日時のセレクトボックス化と現在日時の一括入力
    記事やページの登録日時や公開終了日時、ショッピングカートプラグインの商品登録の発行日、販売終了日時、注文管理プラグインの注文登録の注文日時の欄をセレクトボックスにして、登録画面で現在日時を一括入力するボタンを設置する。
    　https://freo.jp/plugin/smarty/date_select.html
    　https://freo.jp/document/customize/datetime.html
    ※記事やページの公開終了日時や商品の販売終了日時欄は「00秒」に、商品登録の発行日は「0時00分00秒」に固定させるためその部分はセレクトボックスにしていません。

--- freo/js/admin.js
+++ freo/js/admin.js
@@ -13,6 +13,31 @@ $(document).ready(function() {
 		$(this).toggleClass('open');
 	});
 
+	//現在日時設定
+	$('#setDatetime').click(function() {
+		var now = new Date();
+
+		var year   = now.getFullYear();
+		var month  = now.getMonth() + 1;
+		var day    = now.getDate();
+		var hour   = now.getHours();
+		var minute = now.getMinutes();
+		var second = now.getSeconds();
+
+		if (month  < 10) { month  = '0' + month;  }
+		if (day    < 10) { day    = '0' + day;    }
+		if (hour   < 10) { hour   = '0' + hour;   }
+		if (minute < 10) { minute = '0' + minute; }
+		if (second < 10) { second = '0' + second; }
+
+		$('#datetime_year').val(year);
+		$('#datetime_month').val(month);
+		$('#datetime_day').val(day);
+		$('#datetime_hour').val(hour);
+		$('#datetime_minute').val(minute);
+		$('#datetime_second').val(second);
+	});
+
 	//メディア管理（tablesorter非公式フォーク版）
 	$('#media').tablesorter({
 		emptyTo: 'top',
