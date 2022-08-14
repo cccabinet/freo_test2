62aa0cfabb2417eaad173256dd29f8e7e81f7013
cccabinet <chay84@hotmail.com>
2022-08-14 09:29:38 +0900

    バージョン情報の修正
    ・カスタマイズ版をHTML版に差替
    ・カスタマイズ版の表記など（軽微な変更のphpファイルを除く）

--- freo/libs/freo/internals/setup/default.php
+++ freo/libs/freo/internals/setup/default.php
@@ -2,7 +2,7 @@
 
 /*********************************************************************
 
- freo | セットアップ (2022/01/10)
+ freo | セットアップ (2022/08/14)
 
  Copyright(C) 2009-2022 freo.jp
  customized：cccabinet（https://cccabinet.jpn.org/)
